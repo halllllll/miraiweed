e5c797de22170b3fc6e8f29feecaba8bae466fa6
h4lllllll <hjod1172@yahoo.co.jp>
2023-05-15 20:22:15 +0900

    re-refactor to shrink, simple `func::main`

--- main.go
+++ main.go
@@ -25,7 +25,31 @@ var (
 )
 
 func hello() {
-	// miraiseed instance number prompt
+	// This Program Requires some neccesary files.
+	// And, there are some steps to assumed, prerequisite settings
+
+	// 0.1 -  For stdout and logs
+	P = ready.NewPut()
+	P.LoggingSetting("miraiweed.log")
+
+	// 0.2 - prepare almost all of paths and naming roles for this project
+	paths, err = ready.NewPATHs()
+	if err != nil {
+		P.ErrLog.Fatal(err)
+	}
+
+	// 1.0 -  To login, ofcourse there are User ID/PW or going along with such info.
+	if _, err := os.Stat(paths.LoginInfo); os.IsNotExist(err) {
+		fmt.Println("The CSV file does not exist. Creating a new template...")
+		if err = ready.CreateCsvTemplate(paths.LoginInfo); err != nil {
+			P.StdLog.Fatalf("Failed to create the CSV template: %s", err)
+		}
+		fmt.Println("Template created. Please fill it with data and run the program again.")
+		P.InfoLog.Println("Create CSV Template.")
+		return // over
+	}
+
+	// 2.0 -  miraiseed instance number prompt. cuz miraiseed serving some url-s for bunch of local goverments.
 	var miraiseedX string
 	for {
 		miraiseedX, err = ready.PromptAndRead("enter miraiseedX(1~9): ")
@@ -43,38 +67,24 @@ func hello() {
 		}
 	}
 
+	// OK, so, some settings under here
+
+	// main urls used in this project.
 	urls = ready.NewUrls()
 	urls.PrepareUrl(miraiseedX)
+
 }
 
 func main() {
-	P = ready.NewPut()
-	P.LoggingSetting("miraiweed.log")
-	// init save directory and csv data info
 
-	paths, err := ready.NewPATHs()
-	if err != nil {
-		P.ErrLog.Fatal(err)
-	}
-	currentDir := paths.Cd
-	downloadPath := paths.DLStorage
-	csvFilePath := filepath.Join(currentDir, ready.LoginCsvFileName)
-
-	if _, err := os.Stat(csvFilePath); os.IsNotExist(err) {
-		fmt.Println("The CSV file does not exist. Creating a new template...")
-		if err := ready.CreateCsvTemplate(csvFilePath); err != nil {
-			P.StdLog.Fatalf("Failed to create the CSV template: %s", err)
-		}
-		fmt.Println("Template created. Please fill it with data and run the program again.")
-		P.InfoLog.Println("Create CSV Template.")
-		return
-	}
 	hello()
 
-	if err = os.MkdirAll(downloadPath, 0755); err != nil {
+	if err = os.MkdirAll(paths.DLStorage, 0755); err != nil {
 		P.StdLog.Fatal(err)
 	}
 	Procces(paths)
+	ready.PromptAndRead("Byebye ﾉｼ")
+
 }
 
 func Procces(paths *ready.PATHs) {
@@ -146,5 +156,4 @@ func Procces(paths *ready.PATHs) {
 	}
 	wg.Wait()
 	time.Sleep(2 * time.Second)
-	ready.PromptAndRead("Byebye ﾉｼ")
 }
